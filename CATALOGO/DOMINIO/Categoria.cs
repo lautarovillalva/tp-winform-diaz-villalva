de3007708e5352c14d8c57f46ab88f340a6563d6
Lautaro Villalva <63483572+lautarovillalva@users.noreply.github.com>
2021-04-26 21:09:20 -0300

    Add files via upload

--- CATALOGO/DOMINIO/Categoria.cs
+++ CATALOGO/DOMINIO/Categoria.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace DOMINIO
 {
-    class Categoria
+    public class Categoria
     {
+        public string Nombre { get; set; }
     }
 }
--- CATALOGO/DOMINIO/Marca.cs
+++ CATALOGO/DOMINIO/Marca.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace DOMINIO
 {
-    class Marca
+    public class Marca
     {
+        public string Nombre { get; set; }
     }
 }
